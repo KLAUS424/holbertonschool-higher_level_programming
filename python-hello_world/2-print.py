198b6c7736f771025c707aefa2b5909f349fe86b
Rauf <raufagayev207@gmail.com>
2025-11-16 11:55:10 +0000

    setir uzunlugu xetasin duzeltdim

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-# This script prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+# bu scriptde " dan escape edirik
 
 print("\"Programming is like building a multilingual puzzle")
