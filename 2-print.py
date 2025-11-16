138ad3ba9b96598b15f927299baeafd857e8ae5c
KLAUS424 <118015501+KLAUS424@users.noreply.github.com>
2025-11-16 15:53:03 +0400

    Delete 2-print.py

--- 2-print.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-# This script prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
-
-print("\"Programming is like building a multilingual puzzle")
