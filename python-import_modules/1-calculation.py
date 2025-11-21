c5548f4c7d6266f6eb3ce7c2a88ce2500741c3f5
Rauf <raufagayev207@gmail.com>
2025-11-21 07:52:02 +0000

    pep8 in zibilleri

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,11 +3,14 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 if __name__ == "__main__":
-   sum = add(a, b)
-   print("{:d} + {:d} = {:d}".format(a, b, sum))
-   sub = sub(a, b)
-   print("{:d} - {:d} = {:d}".format(a, b, sub))
-   mul = mul(a, b)
-   print("{:d} * {:d} = {:d}".format(a, b, mul))
-   div = div(a, b)
-   print("{:d} / {:d} = {:d}".format(a, b, div))
+    sum_result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, sum_result))
+
+    sub_result = sub(a, b)
+    print("{:d} - {:d} = {:d}".format(a, b, sub_result))
+
+    mul_result = mul(a, b)
+    print("{:d} * {:d} = {:d}".format(a, b, mul_result))
+
+    div_result = div(a, b)
+    print("{:d} / {:d} = {:d}".format(a, b, div_result))
