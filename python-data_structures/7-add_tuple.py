e5e35a811ecc559a1f50b6e6796766b506dad88f
Rauf <raufagayev207@gmail.com>
2025-11-23 09:55:37 +0000

    2 tuple sum

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    def get_first_two(t):
+
+        if len(t) >= 2:
+            return (t[0], t[1])
+
+        elif len(t) == 1:
+            return (t[0], 0)
+
+        else: 
+            return (0, 0)
+
+
+    a1, a2 = get_first_two(tuple_a)
+    b1, b2 = get_first_two(tuple_b)
+
+
+    first_sum = a1 + b1
+    second_sum = a2 + b2
+
+    return (first_sum, second_sum)
