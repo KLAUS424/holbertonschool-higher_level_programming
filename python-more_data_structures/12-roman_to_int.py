43f22e6310636e0498ef0fed02df4ccc85da0df5
Rauf <raufagayev207@gmail.com>
2025-11-23 12:33:21 +0000

    salam

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_map = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+    total = 0
+    n = len(roman_string)
+    for i in range(n):
+        current_val = roman_map.get(roman_string[i], 0)
+        if i < n - 1:
+            next_val = roman_map.get(roman_string[i+1], 0)
+        else:
+            next_val = 0
+        if current_val < next_val:
+            total -= current_val
+        else:
+            total += current_val
+    return total
