c875b82f62b31614a900af2e77433d8b4368a3c3
Rauf <raufagayev207@gmail.com>
2025-11-23 12:25:03 +0000

    duzelis

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
     best_key = max(a_dictionary.keys(), key=a_dictionary.get)
     return best_key
-
