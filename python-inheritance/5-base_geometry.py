6e7833c1ade763982ca28b45e7d9c484e0b79e1c
Rauf <raufagayev207@gmail.com>
2025-11-29 22:08:04 +0000

    base geometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+5-base_geometry: Contains the empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class serving as the base for geometry related objects.
+    """
+    pass
