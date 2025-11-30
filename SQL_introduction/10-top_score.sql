9c9b64d78556ce89378f033c03e9d4cc828bd899
Rauf <raufagayev207@gmail.com>
2025-11-30 15:22:31 +0000

    update

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Script that lists all records of the table 'second_table'.
+-- Results display the score and the name, ordered by score (descending).
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
