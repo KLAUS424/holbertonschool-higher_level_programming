15c3eb363371c274d285b82b651cbfa2d9b2cc1d
Rauf <raufagayev207@gmail.com>
2025-11-30 15:08:09 +0000

    update

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- Script to list all tables present in a specific database.
+-- The database name is passed as an argument to the mysql command.
+-- SHOW TABLES command automatically displays the tables for the currently selected database.
+SHOW TABLES;
