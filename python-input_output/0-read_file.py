d70782d9ceb3f0384fd53b4c1c48839671150d30
Rauf <raufagayev207@gmail.com>
2025-11-30 10:03:43 +0000

    first

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 Module 0-read_file
-Contains a function to read the contents of a text file and print them to stdout.
+Contains a function to read the contents of a text file
+ and print them to stdout.
 """
 
 
@@ -10,11 +11,12 @@ def read_file(filename=""):
     Reads a text file (UTF8) and prints its content to standard output.
 
     Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
+        filename (str): The name of the file to read.
+          Defaults to an empty string.
 
     Raises:
-        (Does not manage exceptions for file permissions or non-existence,
-         as per requirements. FileNotFoundError, for example, will propagate.)
+     (Does not manage exceptions for file permissions or non-existence,
+     as per requirements. FileNotFoundError, for example, will propagate.)
     """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
