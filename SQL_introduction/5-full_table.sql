e5581067bc8ea660c21706aabb42b4642c5276cc
Rauf <raufagayev207@gmail.com>
2025-11-30 15:16:24 +0000

    update

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Script that prints the full creation statement for the table 'first_table'.
+-- The database name is passed as an argument to the mysql command.
+-- SHOW CREATE TABLE is used to display the exact SQL required to recreate the table.
+SHOW CREATE TABLE first_table;
