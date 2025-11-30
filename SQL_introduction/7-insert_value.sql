0d7bd8ec0dfde9f84b4e31662299a57a9ac943c7
Rauf <raufagayev207@gmail.com>
2025-11-30 15:18:31 +0000

    update

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- Script that inserts a new row into the table 'first_table'.
+-- The row has id = 89 and name = 'Best School'.
+-- The database name is expected to be passed as an argument to the mysql command.
+INSERT INTO `first_table` (`id`, `name`)
+VALUES (89, 'Best School');
